3c109447a6030ad52a7431065f76b9ec5d305b12
My digital passions <laferrierejc@gmail.com>
2019-03-16 23:13:15 -0700

    subset complete

--- cleandatacode.R
+++ cleandatacode.R
@@ -1,34 +1,36 @@
 ## read in data ##
+#select
+library(dplyr)
 
 sourceDir="C:/Users/user/Documents/School/CSUF/ISDS577/projects/Capstone-577/"
 
-data<-read.csv(paste0(sourceDir,"MyData.csv"))
-list<-data.frame(read.csv(paste0(sourceDir,"filterList.txt"), header=FALSE))
+data<-read.csv(paste0(sourceDir,"MyData.csv"),sep=",",quote="\"")
+list<-read.csv(paste0(sourceDir,"filterList.txt"), header=FALSE, sep=,)
 
-dim(data)
+#dim(data)
 # check missing with for loop
 # The below code gives the number of missing values for each variables
-for (ii in 1:ncol(data)) {
-  print( colnames(data)[ii] )
-  print( table(is.na(data[,ii])) )
-}
+
+#expensive, descriptive only
+#for (ii in 1:ncol(data)) {
+#  print( colnames(data)[ii] )
+#  print( table(is.na(data[,ii])) )
+#}
 ## select the columns with no missings
 
 cleandata<-data[,colSums(is.na(data)) == 0] # dat[A, B] takes the A rows and B columns; A and B are indices; 
 # if A or B is not specified, all rows or columns will be retained
-table(is.na(cleandata))# table(is.na(cleandata)) gives the number of missing values of data
+
+#expensive, descriptive function only
+#table(is.na(cleandata))# table(is.na(cleandata)) gives the number of missing values of data
 #Since there are no missing values we export the data
 #write.csv(cleandata, "C:\\Users\\CampusUser\\Desktop\\MyData.csv")
 
 colnames(data)
 
-data
-#subSetted <- data[data %in% list, ]
-
 #https://stackoverflow.com/questions/27556353/subset-columns-based-on-list-of-column-names-and-bring-the-column-before-it
-idx <- match(list, names(data))
 
-#parsedList2 <- parsedList[!parsedList %in% c(filtered)]
-filtered <- c(filter,parsedList[i])
+col.num <- which(colnames(data) %in% as.character(list[,1]))
 
-colnames
+NewDF <- data[,(c(col.num))]
+
